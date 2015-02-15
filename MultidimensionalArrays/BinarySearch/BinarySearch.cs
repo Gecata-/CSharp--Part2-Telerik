2c3b37c23eeb3a85ea7ea8be193c711f0095b104
Georgi Dimitrov <gb_dimitrov@yahoo.com>
2015-02-15 14:10:11 +0200

    finish

---
+++ MultidimensionalArrays/BinarySearch/BinarySearch.cs
@@ -0,0 +1,44 @@
+﻿//Problem 4. Binary search
+
+//Write a program, that reads from the console an array of N integers and an integer K,
+//sorts the array and using the method Array.BinSearch() finds the largest number in the array which is ≤ K.
+using System;
+
+class BinarySearch
+{
+    static void Main()
+    {
+        int currentBigNumber = int.MinValue;
+        int index = 0;
+        //int k = 100;
+        //int[] arr = new int[] { 1, 2, 37, 4, 5, 99, 11, 2, 3, 5, 4, 6, 8, 9 };
+
+        Console.Write(" Enter number of elements for an array: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write("element [{0}]: ", i);
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+        Array.Sort(arr);
+        Console.WriteLine(" Enter number 'K' to test: 'N ≤ K': ");
+        int k = int.Parse(Console.ReadLine());
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] < k && arr[i] > currentBigNumber)
+            {
+                currentBigNumber = arr[i];
+                index = Array.BinarySearch(arr, currentBigNumber);
+            }
+            else if (arr[i] == k)
+            {
+                currentBigNumber = arr[i];
+                index = Array.BinarySearch(arr, currentBigNumber);
+            }
+        }
+        Console.WriteLine(arr[index]);
+    }
+}
+
--- MultidimensionalArrays/MaximalSum/MaximalSum.cs
+++ MultidimensionalArrays/MaximalSum/MaximalSum.cs
@@ -9,13 +9,13 @@ class MaximalSum
     static void Main()
     {
         int n = 8; //int.Parse(Console.ReadLine());
-        int m =11; //int.Parse(Console.ReadLine());
+        int m = 11; //int.Parse(Console.ReadLine());
         Random rnd = new Random();
         int[,] matrix = new int[n, m];
         int maxSum = int.MinValue;
         int sum = 0;
         int startRow = 0;
-        int startCow = 0;
+        int startCol = 0;
 
         for (int row = 0; row < matrix.GetLength(0); row++)
         {
@@ -23,30 +23,42 @@ class MaximalSum
             {
                 matrix[row, col] = row + col;//rnd.Next(0, 80);
             }
-            
+
         }
-        for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+        for (int row = 0; row < matrix.GetLength(0) - 2; row++)
         {
-            for (int col = 0; col < matrix.GetLength(1); col++)
+            for (int col = 0; col < matrix.GetLength(1) - 2; col++)
             {
-                sum = matrix[]
-                if (true)
+                sum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2] +
+                    matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2] +
+                    matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+
+                if (sum > maxSum)
                 {
-                    
+                    maxSum = sum;
+                    startRow = row;
+                    startCol = col;
                 }
             }
 
         }
-        
+        Console.Write(matrix[startRow, startCol] + " " + matrix[startRow, startCol + 1] + " " + matrix[startRow, startCol + 2]);
+        Console.WriteLine();
+        Console.Write(matrix[startRow + 1, startCol] + " " + matrix[startRow + 1, startCol + 1] + " " + matrix[startRow + 1, startCol + 2]);
+        Console.WriteLine();
+        Console.Write(matrix[startRow + 2, startCol] + " " + matrix[startRow + 2, startCol + 1] + " " + matrix[startRow + 2, startCol + 2]);
+        Console.WriteLine();
+        Console.WriteLine("The max sum is: {0} " ,maxSum);
+        Console.WriteLine();
 
-        for (int row = 0; row < matrix.GetLength(0)-1; row++)
+        for (int row = 0; row < matrix.GetLength(0); row++)
         {
             for (int col = 0; col < matrix.GetLength(1); col++)
             {
-                Console.Write("{0,4} ", matrix[row,col]);
+                Console.Write("{0,4} ", matrix[row, col]);
             }
             Console.WriteLine();
-        } 
+        }
     }
 }
 
