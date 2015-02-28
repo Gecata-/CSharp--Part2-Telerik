8683cdcb60290e5ef9152a586abbbf9fbb9f1aca
Gecata- <gb_dimitrov@yahoo.com>
2015-02-28 23:25:38 +0200

    solved 07 tomorrow more

--- TextFiles/6. SaveSortedNames/06. SaveSortedNames.cs
+++ TextFiles/6. SaveSortedNames/06. SaveSortedNames.cs
@@ -11,16 +11,30 @@
 // George	         Peter
 
 using System;
+using System.Collections.Generic;
 using System.IO;
+
 
 class SaveSortedNames
 {
     static void Main()
     {
         var streamReader = new StreamReader(@"..\..\input.txt");
-        string text = streamReader.ReadToEnd();
-        string[] input = text.Split(' ');
-
+        string currentName = streamReader.ReadLine();
+        List<string> textNames = new List<string>();
+        while (currentName != null)
+        {
+            textNames.Add(currentName);
+            currentName = streamReader.ReadLine();
+        }
+        textNames.Sort();
+        var streamWriter = new StreamWriter(@"..\..\output.txt");
+        for (int i = 0; i < textNames.Count; i++)
+        {
+            streamWriter.WriteLine(textNames[i].Trim());
+        }
+        streamReader.Close();
+        streamWriter.Close();
     }
 }
 
